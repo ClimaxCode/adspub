ff0dd2c016c4fc2f52dc0b5ec64737c99f0dabb4
hp <climaxcodeapps@gmail.com>
2024-11-20 11:05:02 +0500

    Push

--- app/src/main/java/com/climax/code/NativeAdsCallActivity.kt
+++ app/src/main/java/com/climax/code/NativeAdsCallActivity.kt
@@ -17,6 +17,7 @@ class NativeAdsCallActivity : AppCompatActivity() {
 
         if (intent.getStringExtra("style").equals("one")){
             binding.one.visibility = View.VISIBLE
+
             callNativeAd("ca-app-pub-3940256099942544/1044960115","large",false,true,{},{},{})
         } else   if (intent.getStringExtra("style").equals("two")){
             binding.two.visibility = View.VISIBLE
